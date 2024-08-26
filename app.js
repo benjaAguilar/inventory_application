f75722f2f66318d887db0604f1c9c00878998341
Benja Aguilar <benjaaguilar02@gmail.com>
2024-08-26 11:03:57 -0300

    chore: config app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('node:path');
+const app = express();
+const indexRouter = require('./routes/indexRoute');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+
+const PORT = 8000;
+app.listen(PORT, () => console.log('server running on port 8000'));
