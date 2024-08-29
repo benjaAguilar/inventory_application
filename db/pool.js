a4d671cd6939e9f467b67289acf6cd70e1126ea4
Benja Aguilar <benjaaguilar02@gmail.com>
2024-08-29 11:39:10 -0300

    ref: pool

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  host: process.env.HOST,
-  user: process.env.USER_NAME,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false 
   }  
